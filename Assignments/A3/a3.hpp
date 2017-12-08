506e44be82ba592237add05eb7e3bd2c49c2bd21
Avinash <ee12b1014@iith.ac.in>
2017-12-08 11:48:48 -0500

    A3

--- Assignments/A3/a3.hpp
+++ Assignments/A3/a3.hpp
@@ -5,16 +5,43 @@
 
 #ifndef A3_HPP
 #define A3_HPP
+#include <math.h>
 
-__global__ void mykernel(){
-  printf("Hello from mykernel\n");
+// __device__
+// float computek(float v){
+//     float v1= powf(v,2);
+//     float v2 = expf(-v1/2);
+//     float v3 = sqrtf(2*M_PI);
+//     float v4 = v2/v3;
+//     return v4;
+// }
+
+__global__
+void evaluate(float *x, float *y, int n, float h){
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    printf("%d\n",x[i]);
+
 }
 
 void gaussian_kde(int n, float h, const std::vector<float>& x, std::vector<float>& y) {
     printf("Hello....\n");
-    mykernel<<<1,1>>>();
-    cudaDeviceSynchronize();
+    int m = 32;
+
+    float *deviceX, *deviceY;
+
+    int size = n*sizeof(float);
+
+    cudaMalloc(&deviceX, size);
+    cudaMalloc(&deviceY, size);
+
+    cudaMemcpy(&deviceX, &x, size, cudaMemcpyHostToDevice);
+
+    evaluate<<<1,1>>>(deviceX, deviceY,n,h);
+
     printf("End!!!!!!!!!\n");
+    for (int i = 0; i < 5; i++) {
+        cout<<x[i]<<"\t"<<y[i]<<endl;
+    }
 } // gaussian_kde
 
 #endif // A3_HPP
