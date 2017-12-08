06d19df266ab4b6110001d82ec8aaa98038d7f7c
Avinash <ee12b1014@iith.ac.in>
2017-12-08 13:51:19 -0500

    A3

--- Assignments/A3/a3.hpp
+++ Assignments/A3/a3.hpp
@@ -17,54 +17,26 @@ void evaluate(float *x, float *y, int n, float h){
 }
 
 void gaussian_kde(int n, float h, const std::vector<float>& x, std::vector<float>& y) {
+    printf("Hello....\n");
+    int m = 32;
 
-    float *d_x,*d_y;
-    int m = 16;
+    float *deviceX, *deviceY;
 
     int size = n*sizeof(float);
 
-    cudaMalloc(&d_x, n*sizeof(float));
-    cudaMalloc(&d_y, n*sizeof(float));
+    cudaMalloc(&deviceX, size);
+    cudaMalloc(&deviceY, size);
 
-    cudaMemcpy(d_x, &x, size , cudaMemcpyHostToDevice);
+    cudaMemcpy(deviceX, &x, size, cudaMemcpyHostToDevice);
 
-    dim3 dimGrid((int)ceil(n/m), (int)ceil(n/m));
-    dim3 dimBlock(m,m);
+    evaluate<<<(int)ceil(n/m),m>>>(deviceX, deviceY,n,h);
 
-    evaluate<<<dimGrid, dimBlock>>>(d_x, d_y, n, h);
+    cudaMemcpy(&y, deviceY, size, cudaMemcpyDeviceToHost);
 
-    cudaMemcpy(&y, d_y, size , cudaMemcpyDeviceToHost);
-
-    for(int i =0;i<n;i++) cout<<y[i]<<"\n";
-
-    cudaFree(d_x);
-    cudaFree(d_y);
-
+    printf("End!!!!!!!!!\n");
+    for (int i = 0; i < 5; i++) {
+        cout<<x[i]<<"\t"<<y[i]<<endl;
+    }
 } // gaussian_kde
 
 #endif // A3_HPP
-
-// void gaussian_kde(int n, float h, const std::vector<float>& x, std::vector<float>& y) {
-//     printf("Hello....\n");
-//     int m = 32;
-//
-//     float *deviceX, *deviceY;
-//
-//     int size = n*sizeof(float);
-//
-//     cudaMalloc(&deviceX, size);
-//     cudaMalloc(&deviceY, size);
-//
-//     cudaMemcpy(deviceX, &x, size, cudaMemcpyHostToDevice);
-//
-//     evaluate<<<(int)ceil(n/m),m>>>(deviceX, deviceY,n,h);
-//
-//     cudaMemcpy(&y, deviceY, size, cudaMemcpyDeviceToHost);
-//
-//     printf("End!!!!!!!!!\n");
-//     for (int i = 0; i < 5; i++) {
-//         cout<<x[i]<<"\t"<<y[i]<<endl;
-//     }
-// } // gaussian_kde
-//
-// #endif // A3_HPP
