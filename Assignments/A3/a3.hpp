3a117e96fdec9c2d5de166e4bccc4394620406c7
Avinash <ee12b1014@iith.ac.in>
2017-12-08 13:19:48 -0500

    A3

--- Assignments/A3/a3.hpp
+++ Assignments/A3/a3.hpp
@@ -9,15 +9,24 @@
 
 using namespace std;
 
-__global__ void evaluate(float *x, float *y, int n, float h){
- int i = blockIdx.x*blockDim.x + threadIdx.x;
+// _device_
+// float computek(float v){
+//     float v1= powf(v,2);
+//     float v2 = expf(-v1/2);
+//     float v3 = sqrtf(2*M_PI);
+//     float v4 = v2/v3;
+//     return v4;
+// }
+
+_global_ void evaluate(float *x, float *y, int n, float h){
+   int i = blockIdx.x*blockDim.x + threadIdx.x;
     if(i<n) y[i] = x[i];
 
 }
 
 void gaussian_kde(int n, float h, const std::vector<float>& x, std::vector<float>& y) {
     printf("Hello....\n");
-    int m = 4;
+    int m = 32;
 
     float *deviceX, *deviceY;
 
@@ -28,7 +37,7 @@ void gaussian_kde(int n, float h, const std::vector<float>& x, std::vector<float
 
     cudaMemcpy(deviceX, &x, size, cudaMemcpyHostToDevice);
 
-    evaluate<<<(int)ceil(n/m),m,m>>>(deviceX, deviceY,n,h);
+    evaluate<<<(int)ceil(n/m),m>>>(deviceX, deviceY,n,h);
 
     cudaMemcpy(&y, deviceY, size, cudaMemcpyDeviceToHost);
 
