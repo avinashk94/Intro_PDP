89a4bfcacbe69426096e3402b41d763756e178bd
Avinash <ee12b1014@iith.ac.in>
2017-12-08 21:42:11 -0500

    A3

--- Assignments/A3/a3.hpp
+++ Assignments/A3/a3.hpp
@@ -20,17 +20,16 @@ __global__ void evaluate(float *x, float *y, int n, float h,float A){
     float k = 0.0;
     float xi = x[i];
 
-    // for (int i = 0; i < gridDim.x; i++) {
-    //     Xs[idx] = x[i*m + idx];
-    //     __syncthreads();
-    //     for (int j = 0; i < m; i++) {
-    //         float a = (xi - x[j])/h;
-    //         k += expf(-powf(a,2));
-    //     }
-    //     __syncthreads();
-    // }
-    y[i] = A*k;
-    y[i] = xi;
+    for (int i = 0; i < gridDim.x; i++) {
+        Xs[idx] = x[i*m + idx];
+        __syncthreads();
+        for (int j = 0; i < m; i++) {
+            float a = (xi - x[j])/h;
+            k += expf(-powf(a,2));
+        }
+        __syncthreads();
+    }
+    y[i] = k;
 }
 
 void gaussian_kde(int n, float h, std::vector<float>& x, std::vector<float>& y) {
@@ -58,7 +57,7 @@ void gaussian_kde(int n, float h, std::vector<float>& x, std::vector<float>& y) 
            float a = (x[j] - x[i])/h;
            k += expf(-powf(a,2));
        }
-       y2[j] = A*k;
+       y2[j] = k;
    }
 
 
