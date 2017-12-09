5caf4b6ae3595f7524222836dea6697dfaf66938
Avinash <ee12b1014@iith.ac.in>
2017-12-09 00:54:38 -0500

    A3

--- Assignments/A3/a3.hpp
+++ Assignments/A3/a3.hpp
@@ -26,7 +26,7 @@ __global__ void evaluate(float *x, float *y, int n, float h,float A){
             if(l*m + idx < n){
                 Xs[idx] = x[l*m + idx];
                 __syncthreads();
-                for (int j = 0; j < m && (l*m + j)<=n; j++) {
+                for (int j = 0; j < m && (l*m + j)<n; j++) {
                     float a = (xi - Xs[j])/h;
                     k += expf(-powf(a,2));
                 }
