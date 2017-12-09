32de92a03ca6d2d1c1c7122518885c5f2e33055c
Avinash <ee12b1014@iith.ac.in>
2017-12-09 17:12:58 -0500

    A3

--- Assignments/A3/a3.hpp
+++ Assignments/A3/a3.hpp
@@ -28,6 +28,7 @@ __global__ void evaluate(float *x, float *y, int n, float h,float A){
             Xs[idx] = x[l*m + idx];
             __syncthreads();
             for (int j = 0; j < m && (l*m + j<n); j++) {
+                printf("cur = %f - b[%d] = %f\n",xi,j,Xs[j]);
                 float a = (xi - Xs[j])/h;
                 k += expf(-powf(a,2));
                 // k += 1;
